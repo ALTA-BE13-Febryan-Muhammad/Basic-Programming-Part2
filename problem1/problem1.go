8ff00e99519daf00cfe4b74e7df84a964a46a322
febryanmz <febryan.zainal@gmail.com>
2022-10-28 01:07:43 +0700

    first assign

---
+++ problem1/problem1.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func KonversiNilai(nilai int) string {
+	// your code here
+	var NilaiUjian string
+	if nilai >= 90 && nilai <= 100 {
+		NilaiUjian = "A"
+	} else if nilai >= 80 && nilai < 90 {
+		NilaiUjian = "B"
+	} else if nilai >= 70 && nilai < 80 {
+		NilaiUjian = "C"
+	} else if nilai >= 0 && nilai < 70 {
+		NilaiUjian = "D"
+	}
+	return NilaiUjian
+}
+
+func main() {
+	var nilai int = 80
+	// var nilai2 int = 34
+	// var nilai3 int = 69
+	// var nilai4 int = 77
+	// var nilai5 int = 99
+
+	fmt.Println(KonversiNilai(nilai))
+	// fmt.Println(KonversiNilai(nilai2))
+	// fmt.Println(KonversiNilai(nilai3))
+	// fmt.Println(KonversiNilai(nilai4))
+	// fmt.Println(KonversiNilai(nilai5))
+}
